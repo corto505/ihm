fcd322474b89e6a89c57b7821d7805dea64695c4
corto505 <eric.balinfo@gmail.com>
2014-05-14 12:06:28 +0200

    css meteo et bouton tdb + angual meteo

--- application/views/meteo_ajx_vw.php
+++ application/views/meteo_ajx_vw.php
@@ -12,15 +12,22 @@
 
 
 <div class="content" ng-controller="ctrlMeteo" id="tabMeteo">
-    <a href="/ihm/index.php/welcome/meteo_api/caen/file" class="btn btn-large btn-green" type="button"> <span class="glyphicon glyphicon-2x glyphicon-refresh"> </span></a>
 
-    <div class="row">
+    <a href="/ihm/index.php/welcome/meteo_api/caen/file" class="btn btn-large btn-green" type="button"> <span class="glyphicon glyphicon-2x glyphicon-refresh"> </span></a>
+     <button class="btn btn-info btn-lg" ng:click="filtre_tps = '6:00:00'" >6:00</button>
+     <button class="btn btn-info btn-lg" ng:click="filtre_tps = '9:00:00'" >9:00</button>
+     <button class="btn btn-info btn-lg" ng:click="filtre_tps = '12:00:00'" >12:00</button>
+     <button class="btn btn-info btn-lg" ng:click="filtre_tps = '15:00:00'" >15:00</button>
+     <button class="btn btn-info btn-lg" ng:click="filtre_tps = '18:00:00'">18:00</button>
+     <button class="btn btn-info btn-lg" ng:click="filtre_tps = '21:00:00'" >21:00</button>
+
+    <div class="row" ng:init="filtre_tps = '12:00:00'">
       
-       <div class="col-xs-3 col-md-3" ng:repeat="item in lameteo.list  | filter :{ dt_txt : '12:00:00' }" >
-                
+       <div class="col-xs-3 col-md-3" ng:repeat="item in lameteo.list  | filter :{ dt_txt : filtre_tps }" >
+             
             <div class="cadre_meteo">
                 <div class="bandeau_date">
-                    <span> {{item.dt_txt}}</span>
+                    <span> {{item.dt_txt}} </span>
                 </div>
                 <div class="bandeau">
                      <span> temp. mini : {{item.main.temp_min}} </span> - <span> maxi : {{item.main.temp_max}} </span>
@@ -29,7 +36,7 @@
                 </div>
                 <div class="content">
                     <div class="temp">
-                        <span >Temp. : {{item.main.temp}}°</span><br>
+                        <span  ng:class=" {'text-info' : item.main.temp < 18 , 'text-danger' : item.main.temp >= 18  }">Temp. : {{item.main.temp}}°</span><br>
                     </div>
                     <img src="<?php echo img_url('weather/{{item.weather[0].icon}}.png') ?>" class="img-responsive" />
                     <p>{{item.weather[0].description}}</p>
