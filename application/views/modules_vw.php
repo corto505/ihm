7b96e2577ee14e436ea2ef42177166bf1b787242
corto505 <eric.balinfo@gmail.com>
2014-06-22 23:02:49 +0200

    Update modules_vw.php

--- application/views/modules_vw.php
+++ application/views/modules_vw.php
@@ -1,16 +1,15 @@
  <?php $this->load->view('include/entete'); ?>
- <h3 class="text-center"> <?php echo $leType ;?></h3>
 <div class="content" ng-controller="ctrlModules">
     <div class="row" ng-init="filterOptions ={val : '<?php echo $leType ;?>'}">
 
     	 <div class="col-xs-12 col-md-4" ng:repeat="item in lesmodules  | filter :{ Type : filterOptions.val }" >
                 
-            <div class="inter">
+            <div class="btn_std btn-green button">
                 <div class="bandeau">
                     <span> {{item.Name}} </span>
                 </div>
-                <div class="content" >
-                    <img src="<?php echo img_url('lampe.jpg'); ?>" />
+                <div class="content_std" >
+                    <span class="glyphicon glyphicon-3x glyphicon-off"></span>
                     <div class="bouttons">
                         <button class="bnt btn-success btn-lg btn_appareil" type="button" typebtn="On" idbtn="{{item.idx}}">On</button><br><br>
                         <button class="bnt btn-danger btn-lg btn_appareil" type="button" typebtn="Off" idbtn="{{item.idx}}">Off</button>
